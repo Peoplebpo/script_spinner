176edc9f095986f55ac1d83861e63893c432e3b3
German Sanhueza <gssalass@gmail.com>
2022-03-25 14:36:50 -0300

    se creo la maqueta del script

---
+++ js/divs.js
@@ -0,0 +1,15 @@
+function mostrar() {
+            div = document.getElementById('flotante');
+            div.style.display = '';
+
+            div = document.getElementById('inicial');
+            div.style.display = 'none';
+        }
+
+        function cerrar() {
+            div = document.getElementById('flotante');
+            div.style.display = 'none';
+
+            div = document.getElementById('inicial');
+            div.style.display = '';
+        }
